ed67e9e5dd526c9a8796ab31435aa4e2196c7b62
shaharcc <100072700+shaharcc@users.noreply.github.com>
2023-04-05 14:47:37 +0300

    Update tournament.c

--- tournament.c
+++ tournament.c
@@ -21,7 +21,7 @@
 #define RANK 4
 #define NUM_OF_COMPONENTS 5
 
-struct tournament_t{
+struct tournament_t {
     int* id;
     char* location;
     int winner_id;
@@ -30,20 +30,17 @@ struct tournament_t{
     Map games;
 };
 
-//get the number of players that have played in this tournament
-static int getPlayersNum(Map players, int tournament_id)
-{
+// get the number of players that have played in this tournament
+static int getPlayersNum(Map players, int tournament_id) {
     int counter = 0; 
     Player curr_player;
     Participance curr_participance;
-    MAP_FOREACH(int*, player_iter, players)
-    {
+    MAP_FOREACH(int*, player_iter, players) {
         curr_player = mapGet(players, player_iter); //check if player_iter is without &
-        MAP_FOREACH(int*, participance_iter, playerGetParticipances(curr_player))
-        {
+        MAP_FOREACH(int*, participance_iter, playerGetParticipances(curr_player)) {
             curr_participance = mapGet(playerGetParticipances(curr_player), participance_iter);
             int* curr_id = participanceGetId(curr_participance);
-            if(idCompare(curr_id, &tournament_id) == 0){
+            if(idCompare(curr_id, &tournament_id) == 0) {
                 counter++;
             }
         free(participance_iter);
@@ -54,14 +51,12 @@ static int getPlayersNum(Map players, int tournament_id)
     return counter;   
 }
 
-//get the average game time of all the games in the tournament
-static double getAverageGameTime(Tournament tournament, int* longest_time, int* games_num)
-{
+// get the average game time of all the games in the tournament
+static double getAverageGameTime(Tournament tournament, int* longest_time, int* games_num) {
     int total_play_time = 0;
     int current_play_time = 0;
     Game curr_game;
-    MAP_FOREACH(int*, game_iter, tournament->games)
-    {
+    MAP_FOREACH(int*, game_iter, tournament->games) {
         curr_game = mapGet(tournament->games, game_iter);
         (*games_num)++;
         current_play_time = gameGetPlayTime(curr_game);
@@ -75,37 +70,30 @@ static double getAverageGameTime(Tournament tournament, int* longest_time, int* 
     return (double)(total_play_time / *games_num);
 }
 
-int tournamentGetMaxGamesForPlayer(Tournament tournament)
-{
+int tournamentGetMaxGamesForPlayer(Tournament tournament) {
     return tournament->max_games_for_player; 
 }
 
-bool gameCheckIfInTournament(Tournament tournament, int game_id)
-{
+bool gameCheckIfInTournament(Tournament tournament, int game_id) {
     if(mapContains(tournament->games, &game_id)){
         return true;
     }
     return false;
 }
 
-Map tournamentGetGames(Tournament tournament)
-{
+Map tournamentGetGames(Tournament tournament) {
     return tournament->games;
 }
 
-int* tournamentGetId(Tournament tournament)
-{
+int* tournamentGetId(Tournament tournament) {
     return tournament->id;
 }
 
-int idCompare(MapKeyElement id1, MapKeyElement id2)
-{
+int idCompare(MapKeyElement id1, MapKeyElement id2) {
     return (*(int*)id1 - *(int*)id2);
 }
 
-
-MapKeyElement idCopy(MapKeyElement id)
-{
+MapKeyElement idCopy(MapKeyElement id) {
     int* copied_id = malloc(sizeof(*copied_id));
     if (copied_id == NULL)
         return NULL;
@@ -113,34 +101,25 @@ MapKeyElement idCopy(MapKeyElement id)
     return (MapKeyElement)copied_id;
 }
 
-
-void idFree(MapKeyElement id)
-{
+void idFree(MapKeyElement id) {
     free(id);
 }
 
-
-
-
-bool tournamentValidateId (int tournament_id)
-{
+bool tournamentValidateId (int tournament_id) {
     if (tournament_id < 0){
         return false;
     }
     return true;
 }
 
-
 void winnerIdUpdate(Map tournaments, int tournament_id, int winner_id){
-    //all the conditions are checked in the mother-function
+    // all conditions are checked in the mother-function
     Tournament tournament = mapGet(tournaments, &tournament_id);
     tournament->winner_id = winner_id;
 }
 
-
-//get the winner id by making the calculation of it score
-static int CalculateWinnerId(double** player_rank, int size)
-{
+// get the winner id by making the calculation of it score
+static int CalculateWinnerId(double** player_rank, int size) {
     int winner_index = 0;
     double max_rank = 0;
     for(int i = 0; i < size; i++){
@@ -163,8 +142,7 @@ static int CalculateWinnerId(double** player_rank, int size)
                         winner_index =i;
                         max_rank = player_rank[winner_index][RANK];
                     }
-                    else
-                    {
+                    else {
                         continue;
                     }
                 }
@@ -172,36 +150,31 @@ static int CalculateWinnerId(double** player_rank, int size)
                     continue;
                 }
             }
-            else{
+            else {
                 continue;
             }
         }
-        else{
+        else {
             continue;
         }
     }
     return player_rank[winner_index][ID];
 }
 
-
-//sets the array of the players rank by putting in the data of their ids, numbers of wins, losses and draws.
-static void arraySet(double** player_rank, Map players, int tournament_id)
-{
+// sets the array of the players rank by putting in the data of their ids, numbers of wins, losses and draws
+static void arraySet(double** player_rank, Map players, int tournament_id) {
     int counter = 0;
     Player curr_player;
     Participance curr_participance;
     Map player_participances;
-    MAP_FOREACH(int*, player_iter, players)
-    {
+    MAP_FOREACH(int*, player_iter, players) {
         curr_player = mapGet(players, player_iter);
         assert(curr_player != NULL);
         player_participances = playerGetParticipances(curr_player);
         assert(player_participances != NULL);
-        MAP_FOREACH(int*, participance_iter, player_participances)
-        {
+        MAP_FOREACH(int*, participance_iter, player_participances)  {
             curr_participance = mapGet(player_participances, participance_iter);
-            if(idCompare(participance_iter, &tournament_id) == 0)
-            {
+            if(idCompare(participance_iter, &tournament_id) == 0) {
                 int* id = participanceGetId(curr_participance);
                 player_rank[counter][ID] = *id;
                 int wins = participanceGetWins(curr_participance);
@@ -221,9 +194,7 @@ static void arraySet(double** player_rank, Map players, int tournament_id)
     }
 }
 
-
-ChessResult printStatistics(Map players, char* path_file, Tournament tournament) //Add checks for file functions
-{
+ChessResult printStatistics(Map players, char* path_file, Tournament tournament) {
     FILE* statistics = fopen(path_file, "w");
     if (statistics == NULL)
         return CHESS_SAVE_FAILURE; 
@@ -241,43 +212,39 @@ ChessResult printStatistics(Map players, char* path_file, Tournament tournament)
     return CHESS_SUCCESS;
 }
 
-//validate the given location of the tournament. Must start with capital letter that is followed by small letters or spaces.
-static bool tournamentValidateLocation(const char* tournament_location)
-{
-    if (isupper(tournament_location[0]) == false){
+// validate the given location of the tournament. Must start with capital letter that is followed by small letters or spaces.
+static bool tournamentValidateLocation(const char* tournament_location) {
+    if (isupper(tournament_location[0]) == false) {
         return false;
     }
     int len = strlen(tournament_location);
-    for (int i=1; i<len; i++)
-    {
-        if (!(islower(tournament_location[i]) || isspace(tournament_location[i]))){
+    for (int i=1; i<len; i++) {
+        if (!(islower(tournament_location[i]) || isspace(tournament_location[i]))) {
             return false;
         }
     }
     return true;
 }
 
-
 Tournament tournamentCreate(Map tournaments, int tournament_id, int max_games_per_player,
-                            const char* tournament_location, ChessResult* chess_result) 
-{
+                            const char* tournament_location, ChessResult* chess_result) {
     if (tournament_location == NULL){
         *chess_result = CHESS_NULL_ARGUMENT;
         return NULL;
     }
-    if (tournamentValidateId(tournament_id) == false){
+    if (tournamentValidateId(tournament_id) == false) {
         *chess_result = CHESS_INVALID_ID;
         return NULL;
     }
-    if (mapContains(tournaments, &tournament_id) == true){
+    if (mapContains(tournaments, &tournament_id) == true) {
         *chess_result = CHESS_TOURNAMENT_ALREADY_EXISTS; 
         return NULL;
     }
-    if (tournamentValidateLocation(tournament_location) == false){
+    if (tournamentValidateLocation(tournament_location) == false) {
         *chess_result =  CHESS_INVALID_LOCATION;
         return NULL;
     }
-    if (max_games_per_player < 0){
+    if (max_games_per_player < 0) {
         *chess_result = CHESS_INVALID_MAX_GAMES;
         return NULL; 
     }
@@ -290,8 +257,7 @@ Tournament tournamentCreate(Map tournaments, int tournament_id, int max_games_pe
     if (games == NULL){
         return NULL;
     }
-    tournament->games = games; 
-    
+    tournament->games = games;
     tournament->id = malloc(sizeof(*(tournament->id)));
     if (tournament->id == NULL){
         mapDestroy(games);
@@ -301,7 +267,7 @@ Tournament tournamentCreate(Map tournaments, int tournament_id, int max_games_pe
     *(tournament->id) = tournament_id;
 
     tournament->location = malloc(sizeof(*(tournament->location))*strlen(tournament_location)+1);
-    if (tournament->location == NULL){
+    if (tournament->location == NULL) {
         free(tournament->id);
         mapDestroy(games);
         free(tournament);
@@ -315,25 +281,23 @@ Tournament tournamentCreate(Map tournaments, int tournament_id, int max_games_pe
     return tournament; 
 }
 
-MapDataElement tournamentCopy(MapDataElement tournament_to_copy)
-{
+MapDataElement tournamentCopy(MapDataElement tournament_to_copy) {
     if (tournament_to_copy == NULL){
         return NULL;
     }
     Tournament tournament_copy = (Tournament)malloc(sizeof(*tournament_copy));
-    if (tournament_copy == NULL){
+    if (tournament_copy == NULL) {
         return NULL;
     }
     Tournament tournament = tournament_to_copy;
     tournament_copy->games = mapCopy(tournament->games); 
-    if (tournament_copy->games == NULL)
-    {
+    if (tournament_copy->games == NULL) {
         free(tournament_copy);
         return NULL;
     }
     
     tournament_copy->id = malloc(sizeof(*(tournament_copy->id)));
-    if (tournament_copy->id == NULL){
+    if (tournament_copy->id == NULL ){
         mapDestroy(tournament->games);
         free(tournament_copy);
         return NULL;
@@ -341,7 +305,7 @@ MapDataElement tournamentCopy(MapDataElement tournament_to_copy)
     *(tournament_copy->id) = *(tournament->id);
 
     tournament_copy->location = malloc(sizeof(*tournament_copy->location)*strlen(tournament->location)+1);
-    if (tournament_copy->location == NULL){
+    if (tournament_copy->location == NULL) {
         free(tournament_copy->id);
         mapDestroy(tournament_copy->games);
         free(tournament_copy);
@@ -355,8 +319,7 @@ MapDataElement tournamentCopy(MapDataElement tournament_to_copy)
     return (MapDataElement)tournament_copy;
 }
 
-void tournamentDestroy(Tournament tournament)
-{
+void tournamentDestroy(Tournament tournament) {
     free(tournament->location);
     free(tournament->id);
     if(tournament->games != NULL){
@@ -365,20 +328,18 @@ void tournamentDestroy(Tournament tournament)
     free(tournament);
 }
 
-void tournamentEnd(Map tournaments, int tournament_id)
-{   
+void tournamentEnd(Map tournaments, int tournament_id) {   
     Tournament tournament = mapGet(tournaments, &tournament_id);
     tournament->is_still_going = false;
 }
 
 
-double** playersRankArrayCreate(int player_size, int num_of_components)
-{
+double** playersRankArrayCreate(int player_size, int num_of_components) {
     double** array = malloc(sizeof(*array)*player_size);
-    if(array == NULL){
+    if(array == NULL {
         return NULL;
     }
-    for(int i = 0; i < player_size; i++){
+    for(int i = 0; i < player_size; i++) {
         array[i] = malloc(sizeof(double)*num_of_components);
         if(array[i] == NULL){
             for(int j = 0; j < i; j++){
@@ -391,23 +352,20 @@ double** playersRankArrayCreate(int player_size, int num_of_components)
     return array;
 }
 
-void destroyArray(Map map, double** array)
-{
+void destroyArray(Map map, double** array) {
     for(int i = 0; i < mapGetSize(map); i++)
         free(array[i]);
     free(array);
 }
 
 
-bool tournamentCheckIfEnded(Tournament tournament)
-{
+bool tournamentCheckIfEnded(Tournament tournament) {
     if(tournament->is_still_going == false)
         return true;
     return false;
 }
 
-int tournamentCalculateWinnerId(Map tournaments, Map players, int tournament_id)
-{
+int tournamentCalculateWinnerId(Map tournaments, Map players, int tournament_id) {
     int players_size = mapGetSize(players);
     double** player_rank = playersRankArrayCreate(players_size, NUM_OF_COMPONENTS);
     if(player_rank == NULL){
